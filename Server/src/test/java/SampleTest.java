4c9f2f7bfc7e8b5acc52ded2da6f99e71b379c10
schaud3 <schaudhury@walmartlabs.com>
2021-05-14 12:50:16 +0530

    Test framework dependency

---
+++ Server/src/test/java/SampleTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+/**
+ * author: schaud3
+ * 14/05/21
+ */
+@Test
+public class SampleTest {
+    @Test
+    public void sampleTest(){
+        assert true;
+
+        System.out.println("Working");
+    }
+}
